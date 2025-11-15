0ec5557653acf13bf11124ea7c4872f3fcce9824
Lochan Patel <112246458+Lochanpatel@users.noreply.github.com>
2025-11-15 09:48:05 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'test/setupTests.ts',
+  },
+});
+
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
